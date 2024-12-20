9683209880f1273317036ef657817aa061a4c3c6
Hubangyan <bangyan.hu@outlook.com>
2024-12-20 17:40:27 +0800

    初始化

---
+++ admin-common/src/main/java/com/sky/constant/JwtParameterConstant.java
@@ -0,0 +1,5 @@
+package com.sky.constant;
+
+public class JwtParameterConstant {
+
+}
---
+++ admin-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -0,0 +1,43 @@
+package com.sky.config;
+
+
+import com.sky.interceptor.JwtTokenInterceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.List;
+
+/**
+ * 配置类，注册web层相关组件
+ */
+@Configuration
+@Slf4j
+public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+
+    @Autowired
+    private JwtTokenInterceptor jwtTokenInterceptor;
+
+    /**
+     * 注册自定义拦截器
+     *
+     * @param registry
+     */
+    protected void addInterceptors(InterceptorRegistry registry) {
+        log.info("开始注册自定义拦截器...");
+        registry.addInterceptor(jwtTokenInterceptor)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/admin/employee/login");
+    }
+
+}
